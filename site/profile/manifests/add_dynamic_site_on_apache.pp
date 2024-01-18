85e38e8dd28ddacc48791a75be7574841d81e479
malec0101 <malec0101@gmail.com>
2024-01-18 21:25:16 +0300

    teeeeeest

--- site/profile/manifests/add_dynamic_site_on_apache.pp
+++ site/profile/manifests/add_dynamic_site_on_apache.pp
@@ -6,10 +6,4 @@ class profile::add_static_site_on_apache {
       port    => [80, 8081],
       docroot => '/var/www/dynamic'
   }
-  apache::vhost { '192.168.56.7':
-    vhost_name      => '*',
-    virtual_docroot => '/var/www/%-2+',
-    serveraliases   => ['*.dynamic',],
-
-  }
 }
