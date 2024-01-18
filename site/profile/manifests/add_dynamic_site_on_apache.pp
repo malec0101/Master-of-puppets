5dac0b1c4574c9592f00457834ed19b4f43f49d2
malec0101 <malec0101@gmail.com>
2024-01-18 22:47:08 +0300

    twsr

--- site/profile/manifests/add_dynamic_site_on_apache.pp
+++ site/profile/manifests/add_dynamic_site_on_apache.pp
@@ -3,8 +3,8 @@ class profile::add_static_site_on_apache {
     default_vhost => false,
     }
     apache::vhost { '192.168.56.7/dynamic':
-      vhost_name => '192.168.56.7/dynamic'
       port       => 80,
+      vhost_name => '192.168.56.7/dynamic',
       docroot    => '/var/www/dynamic'
   }
 }
