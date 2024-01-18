514a29b1cf615d0dfafdef0886b78eb9a442d508
malec0101 <malec0101@gmail.com>
2024-01-18 16:30:43 +0300

    test static site

--- site/profile/manifests/add_static_site_on_apache.pp
+++ site/profile/manifests/add_static_site_on_apache.pp
@@ -2,9 +2,8 @@ class profile::add_static_site_on_apache {
   class { 'apache':
     default_vhost => false,
   }
-  apache::vhost { 'static':
-  ip       => '192.168.56.6',
-  docroot  => '/var/www/html/static',
-  ip_based => true,
+    apache::vhost { '192.168.56.6':
+    docroot  => '/var/www/html',
+    ip_based => true,
   }
 }
