bbc5a335b5dda615aafab3079ac429f5f9e3ed08
malec0101 <malec0101@gmail.com>
2024-01-18 21:17:13 +0300

    test

--- site/profile/manifests/add_dynamic_site_on_apache.pp
+++ site/profile/manifests/add_dynamic_site_on_apache.pp
@@ -2,15 +2,16 @@ class profile::add_static_site_on_apache {
   class { 'apache':
     default_vhost => false,
     }
-    apache::vhost { '192.168.56.7'
-      port    => 80,
-      docroot => '/var/www/dynamic'
-    }
     apache::vhost { '192.168.56.7/dynamic':
-      port            => 80,
-      vhost_name      => '*'
-      virtual_docroot => '/var/www/%-2+',
-      docroot         => '/var/www'
-      serveraliases   => ['*/dynamic',],
+      port    => [80, 8081],
+      docroot => '/var/www/dynamic'
+  }
+  apache::vhost { '192.168.56.7':
+    vhost_name      => '*',
+    port            => 80,
+    virtual_docroot => '/var/www/%-2+',
+    docroot         => '/var/www',
+    serveraliases   => ['*.dynamic',],
+
   }
 }
