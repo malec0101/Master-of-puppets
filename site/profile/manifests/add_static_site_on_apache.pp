e8f59901da1ed30c0fb2a9a965e5947fec6b78db
malec0101 <malec0101@gmail.com>
2024-01-19 10:40:08 +0300

    static TEST

--- site/profile/manifests/add_static_site_on_apache.pp
+++ site/profile/manifests/add_static_site_on_apache.pp
@@ -2,9 +2,9 @@ class profile::add_static_site_on_apache {
   class { 'apache':
     default_vhost => false,
     }
-    apache::vhost { '192.168.56.6/static':
-      vhost_name      => '192.168.56.6/static'
-      port            => 80,
-      docroot         => '/var/www/static',
-  }
+    apache::vhost { 'static':
+      port       => 80,
+      docroot    => '/var/www/static',
+      servername => '192.168.56.6/static'
+    }
 }
