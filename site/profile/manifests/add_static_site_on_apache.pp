8bc7c2668550da407fe6527d36a75d4d855c2cd6
malec0101 <malec0101@gmail.com>
2024-01-19 11:42:56 +0300

    fix static path

--- site/profile/manifests/add_static_site_on_apache.pp
+++ site/profile/manifests/add_static_site_on_apache.pp
@@ -7,11 +7,11 @@ class profile::add_static_site_on_apache {
       docroot    => '/var/www/static',
       servername => '192.168.56.6/static'
     }
-    file { '/var/www/dynamic/index.php':
+    file { '/var/www/static/index.html':
       ensure  => file,
       owner   => 'apache',
       group   => 'apache',
-      content => file('examples/index.php'),
+      content => file('examples/index.html'),
     }
 }
 
