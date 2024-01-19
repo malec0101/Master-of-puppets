59335c2b04fbd8d1c13d2ce73206231c55a000b9
malec0101 <malec0101@gmail.com>
2024-01-19 11:09:09 +0300

    dynamic template

--- site/profile/manifests/add_dynamic_site_on_apache.pp
+++ site/profile/manifests/add_dynamic_site_on_apache.pp
@@ -2,9 +2,14 @@ class profile::add_dynamic_site_on_apache {
   class { 'apache':
     default_vhost => false,
     }
-    apache::vhost { '192.168.56.7/dynamic':
+    apache::vhost { 'dynamic':
       port       => 80,
-      vhost_name => '192.168.56.7/dynamic',
-      docroot    => '/var/www/dynamic'
+      docroot    => '/var/www/dynamic',
+      servername => '192.168.56.7/dynamic'
+  }
+  file { '/var/www/dynamic/index.php':
+    owner   => 'apache'
+    group   => 'apache'
+    content => file('examples/index.php')
   }
 }
