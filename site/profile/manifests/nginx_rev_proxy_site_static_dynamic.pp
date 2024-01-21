90323f43410fca302b5218d92f7ba1dd0cb3cc79
malec0101 <malec0101@gmail.com>
2024-01-21 19:36:20 +0300

    1

--- site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
+++ site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
@@ -7,10 +7,8 @@ class profile::nginx_rev_proxy_site_static_dynamic {
 
     nginx::resource::location { '/static':
       proxy  => 'http://192.168.56.6:8080',
-      server => '192.168.56.5',
     }
     nginx::resource::location { '/dynamic':
       proxy  => 'http://192.168.56.7',
-      server => '192.168.56.5',
 }
 
