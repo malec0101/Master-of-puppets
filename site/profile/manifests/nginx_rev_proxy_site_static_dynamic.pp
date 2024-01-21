bc0ebd935e420d7a890f4c1dadf97e2cffe23752
malec0101 <malec0101@gmail.com>
2024-01-21 19:10:35 +0300

    test

--- site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
+++ site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
@@ -1,25 +1,13 @@
 class profile::nginx_rev_proxy_site_static_dynamic {
   class { 'nginx':
   }
-    nginx::resource::upstream { 'upstream_app':
-    members => {
-      '192.168.56.6:8080' => {
-        server => '192.168.56.6',
-        port   => 8080,
-      },
-      '192.168.56.7:80' => {
-        server => '192.168.56.7',
-        port   => 80,
-      },
-    }
-  }
-    nginx::resource::server { '192.168.56.5':
+    nginx::re    nginx::resource::server { '192.168.56.5':
       www_root => '/opt/html/',
   }
 
 
-    nginx::resource::location { '/':
-      proxy  => 'http://upstream_app/',
-      server => '192.168.56.5'
+    nginx::resource::location { '/static':
+      proxy  => 'http://192.168.56.6:8080',
+      server => '192.168.56.5',
     }
 }
