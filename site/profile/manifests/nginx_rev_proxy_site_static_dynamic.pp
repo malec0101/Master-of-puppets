8bcc24546c2dc87064bab0b29797e6e6b02df94f
malec0101 <malec0101@gmail.com>
2024-01-22 23:13:37 +0300

    test

--- site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
+++ site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
@@ -1,15 +1,15 @@
 class profile::nginx_rev_proxy_site_static_dynamic {
   include nginx
 
-  nginx::resource::server { 'localhost:8080':
+  nginx::resource::server { '192.168.56.5':
     ensure      => present,
-    listen_port => 8080,
+    listen_port => 80,
     proxy       => 'http://192.168.56.6:8080/static/',
   }
 
-  nginx::resource::server { 'localhost:8081':
+  nginx::resource::server { '192.168.56.5':
     ensure      => present,
-    listen_port => 8081,
+    listen_port => 80,
     proxy       => 'http://192.168.56.7/dynamic/',
   }
 }  
