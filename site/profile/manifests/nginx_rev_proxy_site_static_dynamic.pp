68a8c61ba34ac43c708a90f3d389fdc5d5fa063f
malec0101 <malec0101@gmail.com>
2024-01-22 23:26:19 +0300

    tst

--- site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
+++ site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
@@ -4,13 +4,17 @@ class profile::nginx_rev_proxy_site_static_dynamic {
   nginx::resource::server { '192.168.56.5':
     ensure      => present,
     listen_port => 80,
-    proxy       => 'http://192.168.56.6:8080/static/',
   }
+  nginx::resource::location{'/static':
+    proxy => 'http://192.168.56.6:8080/' ,
+    server => '192.168.56.5',
+  }
+  nginx::resource::location{'/dynamic':
+    proxy => 'http://192.168.56.7/' ,
+    server => 'www.myhost.com',
 
-  nginx::resource::server { '192.168.56.5':
-    ensure      => present,
-    listen_port => 80,
-    proxy       => 'http://192.168.56.7/dynamic/',
   }
-}  
+}
+
+
 
