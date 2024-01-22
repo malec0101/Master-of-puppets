e8b0df47bfba91ad3b7b48710319b7fc1fe81ea2
malec0101 <malec0101@gmail.com>
2024-01-22 22:18:57 +0300

    test

---
+++ modules/minecraft_serv/manifests/init.pp
@@ -0,0 +1,3 @@
+class minecraft_serv (
+
+)
--- site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
+++ site/profile/manifests/nginx_rev_proxy_site_static_dynamic.pp
@@ -1,14 +1,17 @@
 class profile::nginx_rev_proxy_site_static_dynamic {
-  class { 'nginx':
-  }
-    nginx::resource::server { '192.168.56.5':
+  include nginx
+
+  nginx::resource::server { 'test.local test':
+    ensure      => present,
+    ipv6_enable => true,
+    proxy       => 'http://proxypass',
   }
 
-
-    nginx::resource::location { '/static':
-      proxy  => 'http://192.168.56.6:8080',
-    }
-    nginx::resource::location { '/dynamic':
-      proxy  => 'http://192.168.56.7',
+  nginx::resource::server { 'test.local:8080':
+    ensure      => present,
+    listen_port => 8080,
+    server_name => ['test.local test'],
+    ipv6_enable => true,
+    proxy       => 'http://proxypass',
 }
 
