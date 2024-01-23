7f190eada8014e7fcbd3c2a0bdb8bd07c8f821e7
malec0101 <malec0101@gmail.com>
2024-01-23 20:43:03 +0300

    ...

--- modules/minecraft_serv/manifests/init.pp
+++ modules/minecraft_serv/manifests/init.pp
@@ -22,5 +22,5 @@ class minecraft_serv (
       source => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
       require => Package['java-17-openjdk']
     }
-    include minecraft_serv::minecraf_systd_servise
+    include minecraft_serv::minecraft_systd_servise
   }
