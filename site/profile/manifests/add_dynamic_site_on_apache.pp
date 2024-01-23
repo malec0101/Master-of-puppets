7f0a5bc7b631ca0c76fedcd44880f9f9be836cbc
malec0101 <malec0101@gmail.com>
2024-01-23 11:23:39 +0300

    ...

--- site/profile/manifests/add_dynamic_site_on_apache.pp
+++ site/profile/manifests/add_dynamic_site_on_apache.pp
@@ -8,15 +8,15 @@ class profile::add_dynamic_site_on_apache {
 
     apache::vhost { 'dynamic':
       port       => 80,
-      docroot    => '/var/www',
+      docroot    => '/var/www/html',
       servername => '192.168.56.7/dynamic'
   }
-  file { '/var/www/dynamic':
+  file { '/var/www/html/dynamic':
     ensure  => directory,
     owner   => 'apache',
     group   => 'apache',
   }
-  file { '/var/www/dynamic/index.php':
+  file { '/var/www/html/dynamic/index.php':
     ensure  => file,
     owner   => 'apache',
     group   => 'apache',
--- site/profile/manifests/add_static_site_on_apache.pp
+++ site/profile/manifests/add_static_site_on_apache.pp
@@ -4,15 +4,15 @@ class profile::add_static_site_on_apache {
     }
     apache::vhost { 'static':
       port       => [80, 8080],
-      docroot    => '/var/www',
+      docroot    => '/var/www/html',
       servername => '192.168.56.6/static'
     }
-    file { '/var/www/static':
+    file { '/var/www/html/static':
       ensure  => directory,
       owner   => 'apache',
       group   => 'apache',
     }
-    file { '/var/www/static/index.html':
+    file { '/var/www/html/static/index.html':
       ensure  => file,
       owner   => 'apache',
       group   => 'apache',
