9aa044971002dd3703bd64694f1837ee497465a3
malec0101 <malec0101@gmail.com>
2024-02-05 01:51:12 +0300

    test

--- modules/minecraft_serv/manifests/init.pp
+++ modules/minecraft_serv/manifests/init.pp
@@ -2,5 +2,7 @@ class minecraft_serv (
   String $xmx = $minecraft_serv::minecraft_systd_service::xmx,
   String $xms = $minecraft_serv::minecraft_systd_service::xms,
 ) {
-  Class['minecraft_serv::install_java'] -> Class['minecraft_serv::download_minecraft'] -> Class['minecraft_serv::minecraft_systd_service']
+  include minecraft_serv::install_java
+  include minecraft_serv::download_minecraft
+  include minecraft_serv::minecraft_systd_service
 }
