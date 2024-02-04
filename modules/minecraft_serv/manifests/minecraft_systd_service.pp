72e4a36e8c31707a887db245049d89f4047a014b
malec0101 <malec0101@gmail.com>
2024-02-04 23:49:59 +0300

    test

--- modules/minecraft_serv/manifests/minecraft_systd_service.pp
+++ modules/minecraft_serv/manifests/minecraft_systd_service.pp
@@ -1,13 +1,13 @@
 class minecraft_serv::minecraft_systd_service {
-  #$xmx = '1024m'
-  #$xms = '512m'
+  $xmx = '1024m'
+  $xms = '512m'
   file { '/etc/systemd/system/minecraft.service':
     ensure    => file,
     owner     => 'root',
     group     => 'root',
     mode      => '0664',
-    #content   => file('minecraft_serv/minecraft.servise')
-    content => epp('minecraft_serv/minecraf.servise.epp', {'xmx' => '1024m', 'xms' => '512m' }),
+    #content   => file('minecraft_serv/minecraft.servise'),
+    content => epp('minecraft_serv/minecraf.servise.epp'),
   }
   service { 'minecraf':
     ensure   => restarted
