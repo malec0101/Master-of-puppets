13fc452c946142528138ba4d17b29da513292e82
malec0101 <malec0101@gmail.com>
2024-02-05 02:52:49 +0300

    test

--- modules/minecraft_serv/manifests/download_minecraft.pp
+++ modules/minecraft_serv/manifests/download_minecraft.pp
@@ -5,13 +5,6 @@ class minecraft_serv::download_minecraft {
     group  => 'root',
     before => File['/opt/minecraft/eula.txt']
   }
-  file { '/opt/minecraft/eula.txt':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    content => file('minecraft_serv/eula.txt'),
-    require => File['/opt/minecraft']
-  }
   file { '/opt/minecraft/server.jar':
     ensure  => file,
     source  => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
