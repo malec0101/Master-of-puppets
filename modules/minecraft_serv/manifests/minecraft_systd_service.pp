0af3e5071a8c5c035dcbebbccce014c178dec6d3
malec0101 <malec0101@gmail.com>
2024-02-05 01:52:45 +0300

    test

--- modules/minecraft_serv/manifests/minecraft_systd_service.pp
+++ modules/minecraft_serv/manifests/minecraft_systd_service.pp
@@ -1,6 +1,6 @@
 class minecraft_serv::minecraft_systd_service {
-  $xmx = '1024m',
-  $xms = '512m',
+  $xmx = '1024m'
+  $xms = '512m'
   file { '/etc/systemd/system/minecraft.service':
     ensure  => file,
     #content   => file('minecraft_serv/minecraft.servise'),
