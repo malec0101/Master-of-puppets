a6b4f42fcb8866938ece6deebec2ae67328cbaf5
malec0101 <malec0101@gmail.com>
2024-02-05 09:36:17 +0300

    test

--- modules/minecraft_serv/manifests/minecraft_systd_service.pp
+++ modules/minecraft_serv/manifests/minecraft_systd_service.pp
@@ -1,19 +1,25 @@
 class minecraft_serv::minecraft_systd_service {
   $xmx = '1200m'
   $xms = '512m'
-  file { '/etc/systemd/system/minecraft.service':
+  -> file { '/etc/systemd/system/minecraft.service':
     ensure  => file,
     #content   => file('minecraft_serv/minecraft.servise'),
     content => epp('minecraft_serv/minecraft.service.epp'),
-    before  => Service['minecraft'],
   }
-  service { 'minecraft':
-    ensure => running
+  -> service { 'minecraft':
+    ensure => running,
+    enable => true,
   }
-  file { '/opt/minecraft/eula.txt':
+  -> service { 'minecraft':
+    ensure => stopped,
+  }
+  -> file { '/opt/minecraft/eula.txt':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     content => file('minecraft_serv/eula.txt'),
-    require => Service['minecraft']
+  }
+  -> service { 'minecraft':
+    ensure => running,
+  } 
 }
