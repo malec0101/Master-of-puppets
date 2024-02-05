671ee26bf5bb848894d24434d1004e80cd54a315
malec0101 <malec0101@gmail.com>
2024-02-05 10:42:36 +0300

    test

--- modules/minecraft_serv/manifests/minecraft_systd_service.pp
+++ modules/minecraft_serv/manifests/minecraft_systd_service.pp
@@ -1,10 +1,11 @@
 class minecraft_serv::minecraft_systd_service {
   $xmx = '1200m'
   $xms = '512m'
-  -> file { '/etc/systemd/system/minecraft.service':
+   file { '/etc/systemd/system/minecraft.service':
     ensure  => file,
     #content   => file('minecraft_serv/minecraft.servise'),
     content => epp('minecraft_serv/minecraft.service.epp'),
+    before  => Service['minecraft']
   }
   -> service { 'minecraft':
     ensure => running,
